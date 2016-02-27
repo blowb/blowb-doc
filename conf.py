c3ced45f05e3eec30a4bcdbe6ac12bc48216287c
Hong Xu <hong@topbug.net>
2016-02-26 19:29:51 -0800

    Use the "manual" LaTeX documentclass instead of "book"

--- conf.py
+++ conf.py
@@ -273,7 +273,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index_printing', 'Blowb.tex', 'Blowb',
-   'Hong Xu \\textless{}hong@topbug.net\\textgreater{}', 'book'),
+   'Hong Xu \\textless{}hong@topbug.net\\textgreater{}', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
